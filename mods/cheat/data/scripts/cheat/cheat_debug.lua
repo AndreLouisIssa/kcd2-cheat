3f828bcc8bead26fe8dd05ab35710550e6b7e001
Andre Louis Issa <andrelouisissa@gmail.com>
2025-02-12 09:46:17 +1100

    attempt type aggregating tracer

--- mods/cheat/data/scripts/cheat/cheat_debug.lua
+++ mods/cheat/data/scripts/cheat/cheat_debug.lua
@@ -118,7 +118,7 @@ function Cheat:print_all_functions(object)
         local getKeyType = loadstring("return type(" .. key .. ")")
         if getKeyType and getKeyType() == "function" then
             Cheat:logWarn("BEGIN FUNC:" .. key)
-            local func = loadstring("print_method_args(" .. key .. ")")
+            local func = loadstring("print_function_args(" .. key .. ")")
             if func then
                 func()
             end
@@ -161,8 +161,8 @@ function Cheat:setup_hook()
     Cheat:logDebug("hook set")
 end
 
-function Cheat:print_method_args(f)
-    Cheat:logDebug("begin - print_method_args")
+function Cheat:print_function_args(f)
+    Cheat:logDebug("begin - print_function_args")
 
     local calltracer = function (event, line)
         Cheat:logWarn("event=" .. event .. " line=" .. tostring(line))
@@ -180,14 +180,175 @@ function Cheat:print_method_args(f)
             end
         end
     end
-    Cheat:logDebug("tracter function created")
+    Cheat:logDebug("tracer function created")
     debug.sethook(calltracer, "crl")
     Cheat:logDebug("debug hook set")
 
     f()
 
     debug.sethook()
-    Cheat:logDebug("end - print_method_args")
+    Cheat:logDebug("end - print_function_args")
+end
+
+do
+    -- since this uses debug.sethook, none of it is thread safe anyway
+
+    local nop = function() end
+    local sethook = debug.sethook
+    local hook_set = false
+    local function_lookup
+    local aggregate
+
+    local function generate_function_lookup()
+        -- assume most relevant functions are global or directly in global tables
+        function_lookup = {}
+        for k,v in pairs(_G) do
+            if k ~= '_G' then
+                local t = type(v)
+                if t == 'function' then
+                    function_lookup[v] = k
+                elseif t == 'table' then
+                    for j,u in pairs(v) do
+                        t = type(u)
+                        if t == 'function' then
+                            function_lookup[u] = k .. '.' .. j
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    local function lookup_function(info)
+        local key = function_lookup[info.func]
+        if key ~= nil then
+            return '_G.' .. key
+        end
+        -- in case the name is some arbitrary local
+        -- we don't want to confuse it with a global
+        return info.name
+    end
+
+    local function get_type(object)
+        local t = type(object)
+        if t ~= 'table' or t ~= 'userdata' then
+            return t
+        end
+        --assume documentation namespace
+        local meta = getmetatable(object)
+        if meta == nil then
+            return t
+        end
+        local namespace = 'kcd2def*'
+        t = meta.type
+        if _G[t] == meta then
+            return namespace .. t
+        end
+        return namespace .. 'unknown-' .. t
+    end
+
+    local function join_type_set(data,name,datum)
+        if name == '(*temporary)' then
+            name = '...'
+        end
+        if data.types == nil then
+            data.types = {}
+        end
+        local types = data.types[name]
+        if types == nil then
+            types = {}
+            data.types[name] = types
+        end
+        types[get_type(datum)] = true
+    end
+
+    function Cheat:begin_aggregate_function_args(join,group)
+        if hook_set then
+            Cheat:logError('aggregate_function_args active, cannot begin')
+            return
+        end
+        
+        Cheat:logDebug("begin - aggregate_function_args")
+
+        if join == nil then
+            join = join_type_set
+        end
+
+        if group == nil then
+            if function_lookup == nil then
+                generate_function_lookup()
+            end
+            group = lookup_function
+        end
+
+        aggregate = {}
+
+        local calltracer = function()
+            --Cheat:logWarn('call')
+            local info = debug.getinfo(2, "flLnSu")
+            --Cheat:tprint(info)
+            local key = group(info)
+            local data = aggregate[key]
+            if data == nil then
+                data = {}
+                aggregate[key] = data
+                for k,v in pairs(info) do
+                    data[k] = v
+                end
+            end
+            local i = 1
+            while true do
+                local name, value = debug.getlocal(2, i)
+                if name ~= nil then
+                    join(data,name,value)
+                    i = i + 1
+                else
+                    break
+                end
+            end
+        end
+
+        Cheat:logDebug("tracer function created")
+        hook_set = true
+        debug.sethook = nop
+        sethook(calltracer, "c")
+    end
+
+    function Cheat:end_aggregate_function_args()
+        if not hook_set then
+            Cheat:logError('aggregate_function_args not active, cannot end')
+            return
+        end
+
+        sethook()
+        debug.sethook = sethook
+        hook_set = false
+        Cheat:logDebug("end - aggregate_function_args")
+
+        return aggregate
+    end
+
+    function Cheat:test_aggregate_function_args()
+
+        local s, m = pcall(function()
+
+        Cheat:begin_aggregate_function_args()
+        
+        print(1)
+        print(coroutine.running())
+        print(next(_G))
+
+        local a = Cheat:end_aggregate_function_args()
+
+        Cheat:tprint(a)
+
+        end)
+
+        if not s then
+            Cheat:logError(m)
+        end
+
+    end
 end
 
 -- ============================================================================
